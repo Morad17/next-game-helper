249319bef4b4c618a2869fa10ba94fef74ed2608
Morad17 <morad123@hotmail.co.uk>
2023-06-27 16:59:39 +0100

    initial commit

---
+++ app/api/games/[id]/route.js
@@ -0,0 +1,3 @@
+export const GET = async (rez, res) => {
+    console.log("get request");
+}
---
+++ app/api/games/route.js
@@ -0,0 +1,7 @@
+export const GET = async (req, res) => {
+    console.log("Get Request");
+}
+
+export const POST = async (req, res) => {
+    console.log("Post Request");
+}
