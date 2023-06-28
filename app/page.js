bdd5a790667f3575cf805c034e60c847ac9262a5
Morad17 <morad123@hotmail.co.uk>
2023-06-28 11:32:13 +0100

    fixed no display issue, add arrow function

--- app/page.js
+++ app/page.js
@@ -1,4 +1,7 @@
 import Image from 'next/image'
+import RootLayout from './layout'
+import Navbar from '../components/Navbar'
+import Card from '@/components/Card'
 
 export default function Home() {
   return (
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,20 +1,24 @@
 import Link from 'next/link'
 
-export default function Navbar() {
+const Navbar = () => {
+    return (
     <nav className="main-nav">
         <div className="nav-links-logo">
-          <div className="logo">Gamer</div>
-          <ul className="nav-home-links">
-            <Link className="nav-links" href="/">Home</Link>
-            <Link className="nav-links" href="/">All Games</Link>
-            <Link className="nav-links" href="/">All Achievements</Link>
-            
-          </ul>
+            <div className="logo">SS</div>
+            <ul className="nav-home-links">
+                <Link className="nav-links" href="/">Home</Link>
+                <Link className="nav-links" href="/">All Games</Link>
+                <Link className="nav-links" href="/">All Achievements</Link>
+                <Link className="nav-links" href="/">Targets</Link>
+            </ul>
         </div>
         <ul className="nav-user-links">
-          <Link className="nav-links" href="/">Login</Link>
-          <Link className="nav-links" href="/">My Account</Link>
+            <Link className="nav-links" href="/">Login</Link>
+            <Link className="nav-links" href="/">My Account</Link>
         </ul>
 
     </nav>
+    )
 }
+
+export default Navbar
